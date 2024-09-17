d26b12fd85b27f6bb9827515f093ecc064d68446
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-09-17 12:16:51 +0100

    refactor  in main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,11 @@
 (* hilbert = 169.254.220.46 *)
 
-let make_load dest = Lib.Load.of_dest dest
+let make_load =
+  let open Lib.Load in
+  of_dest ~distribution:(Point 3.) (Uri.of_string "http://localhost:3000")
 
 let () =
-  let load = make_load (Uri.of_string "http://localhost:3000") in
+  let load = make_load in
   let handle_res promised_res =
     let open Lwt.Infix in
     promised_res >>= Lib.Request.body_of_res >|= print_endline |> Lwt_main.run
