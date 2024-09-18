acb36de3830ebac0bf6e093e8e07732b90074978
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-09-18 14:57:45 +0100

    sync requester thread with an async request handler on the main thread

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,8 @@
 (* hilbert = 169.254.220.46 *)
+module Serial0 = Serial.Make (struct
+  let port = "/dev/cu.usbmodem2102"
+  let baud_rate = 115200
+end)
 
 let make_load =
   let open Lib.Load in
@@ -24,10 +28,6 @@ let handle_req req =
           (* On promise rejected. *)
             (fun _ -> Lwt.return Oracle.Fail)
   in
-  let module Serial0 = Serial.Make (struct
-    let port = "/dev/stdout"
-    let baud_rate = 115200
-  end) in
   score >|= Oracle.string_of_score >>= Serial0.write_line
 
 let rec listen chan =
@@ -39,9 +39,14 @@ let rec listen chan =
 let () =
   let load = make_load in
   let open Domainslib in
+  (* Create a new OS thread channel to pass messages between this OS thread and any others spawned with [Domain.spawn]. *)
   let main_chan = Chan.make_unbounded () in
-  let _req_handler =
-    let top_promise : 'a Lwt.t = listen main_chan in
-    Domain.spawn (fun _ -> Lwt_main.run top_promise)
-  in
-  Seq.iter (Chan.send main_chan) load
+
+  (* Spawn a new OS thread running a synchronous function that makes requests at the required intervals. *)
+  let requester = Domain.spawn (fun _ -> Seq.iter (Chan.send main_chan) load) in
+
+  (* Use the main OS thread as an asynchronous runtime, handling promised responses that are received over the channel from the [requester] OS thread. *)
+  let _ = Lwt_main.run (listen main_chan) in
+
+  (* Await the termination of the requester thread (which will not terminate for never-ending request loads (that are infitite sequences). *)
+  Domain.join requester
