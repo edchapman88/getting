d19b4f43231276f7dabeeabf4819feff98ac71f4
edchapman88 <echapman@turing.ac.uk>
2024-09-19 19:14:08 +0100

    add conf-libev to project

--- lib/request.ml
+++ lib/request.ml
@@ -13,7 +13,7 @@ let code_of_res res =
   res |> fst |> Cohttp.Response.status |> Cohttp.Code.code_of_status
 
 let body_of_res res = res |> snd |> Cohttp_lwt.Body.to_string
-let s_meta_of_res res = res |> fst |> Cohttp_lwt.Response.sexp_of_t
+let s_meta_of_res res = res |> fst |> Cohttp.Response.sexp_of_t
 let meta_of_res res = res |> s_meta_of_res |> Sexplib0.Sexp.to_string_hum
 
 let send params =
