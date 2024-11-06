39d8fbc79f6db13daf97b7ca0a03abe24a0cc0ad
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-11-06 10:17:48 +0000

    improve comments for serial.mli

--- lib/serial.mli
+++ lib/serial.mli
@@ -1,10 +1,18 @@
-type config = {
-  baud : int;
-  port : string;
-}
-
 type t
+(** A serial connection. *)
+
+type config = {
+  baud : int;  (** Baude rate. *)
+  port : string;
+      (** File system address of the serial device, e.g. "/dev/tty0". *)
+}
+(** Configuration for a connection of type [Serial.t]. *)
 
 val make : config -> t Lwt.t
+(** [make conf] returns a promised [Serial.t] connection, configured by [conf] (of type [config]). *)
+
 val write_line : t -> string -> t Lwt.t
+(** [write_line serial message] writes [message] to the connection [serial], asynchronously, returning the promise of a new connection ([t Lwt.t]). If a write fails, an attempt is made to establish a new connection which is returned in place of the old connection. *)
+
 val write_of_score : t -> Oracle.score Lwt.t -> t Lwt.t
+(** Convenience function to write an [Oracle.score] to a serial connection. *)
