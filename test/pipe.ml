9b53542773b962d3eb8d75ae10cf94b1f76cad5d
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-11-19 18:57:46 +0000

    improve tests for Pipe module

--- test/pipe.ml
+++ test/pipe.ml
@@ -1,9 +1,10 @@
 open Lib
+open Utils
 
 let simulated_promises waits =
   let open Lwt.Infix in
   List.map
-    (fun wait -> Lwt_unix.sleep (float_of_int wait) >|= fun () -> wait)
+    (fun wait () -> Lwt_unix.sleep (float_of_int wait) >|= fun () -> wait)
     waits
 
 let producer_delays delays_s =
@@ -18,16 +19,20 @@ let seq_of delays_and_promises =
     | [] -> failwith "unreachable"
     | p :: rest ->
         promises := rest;
-        p
+        p ()
   in
   Seq.map mapper (producer_delays ds)
 
 let out_of_order () = seq_of [ (0, 5); (0, 1); (0, 3); (0, 2) ]
-let blocking_channel () = seq_of [ (0, 3); (0, 1); (0, 2); (4, 1) ]
+let blocking_channel_end () = seq_of [ (0, 3); (0, 1); (0, 2); (4, 1) ]
 
-let handle_to_list l promise =
-  let open Lwt.Infix in
-  promise >|= fun p -> l := p :: !l
+let blocking_channel_start () =
+  seq_of [ (4, 1); (0, 3); (0, 5); (0, 1); (0, 2) ]
+
+let handle_to_list start_time l promise =
+  promise |> fun p ->
+  l := (p, string_of_delta_t ~round:true start_time ()) :: !l;
+  Lwt.return ()
 
 let printer l =
   let rec aux n =
@@ -36,32 +41,50 @@ let printer l =
       aux (n - 1))
   in
   List.iter
-    (fun n ->
+    (fun (n, time) ->
       aux n;
+      print_string (" " ^ time);
       print_string "\n")
     l
 
-let%expect_test "pipe a sequence of promises that resolve out-of-order" =
+(* The promises should be handled in the order that they resolve rather than their order in the input sequence. *)
+let%expect_test "pipe a sequence of promises that resolve out of order" =
   let record = ref [] in
-  let pipe = Pipe.of_handler (handle_to_list record) in
+  let start_time = Unix.gettimeofday () in
+  let pipe = Pipe.of_handler (handle_to_list start_time record) in
   Pipe.process pipe (out_of_order ());
   printer (List.rev !record);
   [%expect {|
-    @
-    @@
-    @@@
-    @@@@@
+    @ 1.
+    @@ 2.
+    @@@ 3.
+    @@@@@ 5.
     |}]
 
 (* Pipe a sequence of promises that are out of order, immediately followed by delay in the producer thread. The expected behaviour is that the async consumer does not block on the empty channel, and handles the out of order promises promptly, in the order of fulfillment. If the consumer blocks while the channel is empty, all of the out of order promises resolve during the block and they are then handled in order of production (due to the list traversal in the [PromiseBag] implementation). *)
 let%expect_test "ensure the async consumer does not block on an empty channel" =
   let record = ref [] in
-  let pipe = Pipe.of_handler (handle_to_list record) in
-  Pipe.process pipe (blocking_channel ());
+  let start_time = Unix.gettimeofday () in
+  let pipe = Pipe.of_handler (handle_to_list start_time record) in
+  Pipe.process pipe (blocking_channel_end ());
   printer (List.rev !record);
   [%expect {|
-    @
-    @
-    @@
-    @@@
+    @ 1.
+    @@ 2.
+    @@@ 3.
+    @ 5.
+    |}]
+
+let%expect_test "handle an empty channel at the start of a sequence" =
+  let record = ref [] in
+  let start_time = Unix.gettimeofday () in
+  let pipe = Pipe.of_handler (handle_to_list start_time record) in
+  Pipe.process pipe (blocking_channel_start ());
+  printer (List.rev !record);
+  [%expect {|
+    @ 5.
+    @ 5.
+    @@ 6.
+    @@@ 7.
+    @@@@@ 9.
     |}]
