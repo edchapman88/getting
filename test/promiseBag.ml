25e06104eb66a4d0ce2dfdccff88ce8b5574ef52
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-11-19 14:08:34 +0000

    add test for PromiseBag module

---
+++ test/promiseBag.ml
@@ -0,0 +1,32 @@
+open Lib
+
+let never_resolving () = Lwt.wait () |> fst
+
+let partially_resolved_bag () =
+  let rec insert bag promises =
+    match promises with
+    | [] -> bag
+    | p :: ps ->
+        let bag' = PromiseBag.insert bag p in
+        insert bag' ps
+  in
+  insert PromiseBag.empty
+    [
+      never_resolving ();
+      Lwt.return ();
+      never_resolving ();
+      Lwt.return ();
+      Lwt.return ();
+    ]
+
+let%expect_test "filter a bag for resolved promises" =
+  let resolved, pending =
+    PromiseBag.filter_resolved (partially_resolved_bag ())
+  in
+  let string_of_count bag = bag |> PromiseBag.count |> string_of_int in
+  print_endline (string_of_count resolved ^ " resolved promises");
+  print_endline (string_of_count pending ^ " pending promises");
+  [%expect {|
+    3 resolved promises
+    2 pending promises
+    |}]
