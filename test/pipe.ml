cdd38b935a3160e05aec9153c116609a11817eb2
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-11-19 11:44:16 +0000

    test for async consumer blocking on an empty channel

--- test/pipe.ml
+++ test/pipe.ml
@@ -1,15 +1,29 @@
 open Lib
 
-let out_of_order =
-  let open Lwt_unix in
+let simulated_promises waits =
   let open Lwt.Infix in
-  let delays = [ 5; 1; 3; 2 ] in
-  let sleeps =
-    List.map
-      (fun delay -> sleep (float_of_int delay) >|= fun () -> delay)
-      delays
+  List.map
+    (fun wait -> Lwt_unix.sleep (float_of_int wait) >|= fun () -> wait)
+    waits
+
+let producer_delays delays_s =
+  Delay.of_delays_s (List.map float_of_int delays_s)
+
+let seq_of delays_and_promises =
+  List.split delays_and_promises |> fun (ds, ps) ->
+  let promises = ref (simulated_promises ps) in
+  let mapper () =
+    let t = !promises in
+    match t with
+    | [] -> failwith "unreachable"
+    | p :: rest ->
+        promises := rest;
+        p
   in
-  List.to_seq sleeps
+  Seq.map mapper (producer_delays ds)
+
+let out_of_order () = seq_of [ (0, 5); (0, 1); (0, 3); (0, 2) ]
+let blocking_channel () = seq_of [ (0, 3); (0, 1); (0, 2); (4, 1) ]
 
 let handle_to_list l promise =
   let open Lwt.Infix in
@@ -30,11 +44,24 @@ let printer l =
 let%expect_test "pipe a sequence of promises that resolve out-of-order" =
   let record = ref [] in
   let pipe = Pipe.of_handler (handle_to_list record) in
-  Pipe.process pipe out_of_order;
+  Pipe.process pipe (out_of_order ());
   printer (List.rev !record);
   [%expect {|
     @
     @@
     @@@
     @@@@@
+    |}]
+
+(* Pipe a sequence of promises that are out of order, immediately followed by delay in the producer thread. The expected behaviour is that the async consumer does not block on the empty channel, and handles the out of order promises promptly, in the order of fulfillment. If the consumer blocks while the channel is empty, all of the out of order promises resolve during the block and they are then handled in order of production (due to the list traversal in the [PromiseBag] implementation). *)
+let%expect_test "ensure the async consumer does not block on an empty channel" =
+  let record = ref [] in
+  let pipe = Pipe.of_handler (handle_to_list record) in
+  Pipe.process pipe (blocking_channel ());
+  printer (List.rev !record);
+  [%expect {|
+    @
+    @
+    @@
+    @@@
     |}]
