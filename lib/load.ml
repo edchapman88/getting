e710ae9bc84e1812100d3a595f68f147b36a7aa1
Ed Chapman - Turing <edchapmanelc@gmail.com>
2024-11-22 14:36:10 +0000

    set timeouts in a  such 1,000 pending requests is the estimated maximum

--- lib/load.ml
+++ lib/load.ml
@@ -5,6 +5,12 @@ type params = {
   destination : Uri.t;
 }
 
+(** Helper function to evaluate an appropriate request timeout such that the expected maximum number of pending requests is 1,000. *)
+let timeout_of_distr distr =
+  let mean_req_period = Delay.mean_of_distr distr in
+  let mean_req_rate = 1.0 /. mean_req_period in
+  1000.0 /. mean_req_rate
+
 (** [req_params_of_params p] returns [Request.params] from the load parameters [p]. *)
 let req_params_of_params p =
   let req_params : Request.params =
@@ -15,8 +21,11 @@ let req_params_of_params p =
 (** [of_params p] returns a request load of type [Load.t] parameterised by [p] of type [params]. *)
 let of_params p =
   let headers = [ ("connection", "close") ] in
+  let timeout = timeout_of_distr p.distribution in
   let delays = Delay.of_distr p.distribution in
-  let make_request () = Request.send ~headers (req_params_of_params p) in
+  let make_request () =
+    Request.send ~timeout ~headers (req_params_of_params p)
+  in
   Seq.map make_request delays
 
 let of_dest ?(distribution = Delay.Point 10.) destination =
